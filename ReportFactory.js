df6445c56ff030ac1cac45a4ac6f3e8046c72072
James Bush <jbush@developertown.com>
2017-10-24 09:49:37 -0400

    Setup email notification

--- ReportFactory.js
+++ ReportFactory.js
@@ -1,10 +1,38 @@
+const mailgun = require('mailgun.js');
+const cmd = require('node-cmd');
+
 const consoleLogReport = (content) => {
   console.log(content);
 };
 
+const emailReport = (content) => {
+  var mg = mailgun.client({
+    username: 'api',
+    key: process.env.MAILGUN_API_KEY || 'key-yourkeyhere',
+  });
+
+  cmd.get(
+    `curl -s --user 'api:key-3d1a8e444a6546768ed3f072c4770832' \
+      https://api.mailgun.net/v3/sandbox872d5518654c45bfb6c145e6dc2bc058.mailgun.org/messages \
+        -F from='Mailgun Sandbox <postmaster@sandbox872d5518654c45bfb6c145e6dc2bc058.mailgun.org>' \
+        -F to='James <jamesbvsh@gmail.com>' \
+        -F subject='Parse complete!' \
+        -F text="${content}"`,
+
+    (err, data, stderr) => {
+      console.log('EMAIL STATUS')
+      err && console.log('\nerr:', err)
+      data && console.log('\ndata:', data)
+      stderr && console.log('\nstderr:', stderr)
+    }
+  )
+}
+
 const ReporterFactory = () => {
   if (process.argv[3] === 'log')
     return consoleLogReport;
+  else if (process.argv[3] === 'email')
+    return emailReport;
   else
     throw 'No reporting method selected';
 }
--- parse.js
+++ parse.js
@@ -13,9 +13,9 @@ edieParser = () => {
 (function curlToSys(callback) {
   if (process.argv[2] === 'curl') {
     let url = 'www.artistryindy.com/?module=check_availability&property[id]=83354&action=view_unit_spaces&property_floorplan[id]=184396';
-    let flags = '-g -H "Accept: application/html" -H "Content-Type: application/html" -X';
+    let flags = '-g -H "Accept: application/html" -H "Content-Type: application/html" -X GET';
     let target = './modals_html/edie.html';
-    let command = `curl ${flags} GET "${url}" > ${target}`;
+    let command = `curl ${flags} "${url}" > ${target}`;
     cmd.get(command, callback);
   } else {
     callback();
