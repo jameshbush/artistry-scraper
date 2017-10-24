f74586152f6efc0432bf9608119517a97a2dc980
James Bush <jbush@developertown.com>
2017-10-24 10:43:13 -0400

    Fix curl mailgun call

--- ReportFactory.js
+++ ReportFactory.js
@@ -1,4 +1,3 @@
-const mailgun = require('mailgun.js');
 const cmd = require('node-cmd');
 
 const consoleLogReport = (content) => {
@@ -6,17 +5,12 @@ const consoleLogReport = (content) => {
 };
 
 const emailReport = (content) => {
-  var mg = mailgun.client({
-    username: 'api',
-    key: process.env.MAILGUN_API_KEY || 'key-yourkeyhere',
-  });
-
   cmd.get(
-    `curl -s --user 'api:key-3d1a8e444a6546768ed3f072c4770832' \
+    `curl -s --user 'api:${process.env.MAILGUN_API_KEY}' \
       https://api.mailgun.net/v3/sandbox872d5518654c45bfb6c145e6dc2bc058.mailgun.org/messages \
         -F from='Mailgun Sandbox <postmaster@sandbox872d5518654c45bfb6c145e6dc2bc058.mailgun.org>' \
-        -F to='James <jamesbvsh@gmail.com>' \
-        -F subject='Parse complete!' \
+        -F to='${process.env.MAILGUN_MAILING_LIST}' \
+        -F subject='Parse Complete!' \
         -F text="${content}"`,
 
     (err, data, stderr) => {
