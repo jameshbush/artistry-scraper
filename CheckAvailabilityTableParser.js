1ba1ab3f66cb76d4ff175162b3b4d66c998227e5
James Bush <jbush@developertown.com>
2017-10-24 12:11:32 -0400

    Only email best rooms

--- CheckAvailabilityTableParser.js
+++ CheckAvailabilityTableParser.js
@@ -1,9 +1,6 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-const outwardFacing = (n) => (Number(n) % 2 !== 0);
-const topFloor = (n) => (n[0] === '5');
-
 class CheckAvailabilityTableParser {
   constructor({ filePath, reportingStrategy }) {
     this.filePath = filePath;
@@ -11,52 +8,42 @@ class CheckAvailabilityTableParser {
     this.parseModal(); // (callback sets $, unitNums, desirableRooms, reportContent)
   };
 
+  outwardFacing(n) {return Number(n) % 2 !== 0};
+  topFloor(n) {return n[0] === '5'};
+
   parseModal() {
     fs.readFile(this.filePath, 'utf-8', (err, content) => {
       if (err) throw `Cannot ${err.syscall} ${err.path}`;
       if (!content) throw `No content found`;
 
-      this.$ = cheerio.load(content);
+      const $ = cheerio.load(content);
 
-      this.unitNums = this.$('.unit-col.unit .unit-col-text')
+      const available = $('.unit-col.unit .unit-col-text')
         .text()
         .match(/.{1,3}/g) // split into 3 char substrings
         .sort();
 
-      this.desirableRooms = {
-        bestRooms: (
-          this.unitNums
-            .filter(topFloor)
-            .filter(outwardFacing)
-          ),
-        topFloorRooms: (
-          this.unitNums
-            .filter(topFloor)
-        ),
-        outwardFacingRooms: (
-          this.unitNums
-            .filter(outwardFacing)
-        ),
+      const best = available
+        .filter(this.topFloor)
+        .filter(this.outwardFacing)
+
+      const topFloor = available
+        .filter(this.topFloor)
+
+      const outwardFacing = available
+          .filter(this.outwardFacing)
+
+      this.rooms = {
+        available,
+        best,
+        topFloor,
+        outwardFacing,
       };
 
-      this.setReportContent();
-      this.reportingStrategy(this.reportContent);
+      this.reportingStrategy(this.rooms);
     });
   };
 
-  setReportContent() {
-    if (this.unitNums && this.desirableRooms) {
-      this.reportContent = [
-        `Rooms: [${this.unitNums.join(', ')}]`,
-        `Best rooms: [${this.desirableRooms.bestRooms.join(', ')}]`,
-        `5th floor rooms: [${this.desirableRooms.topFloorRooms.join(', ')}]`,
-        `Outward facing rooms: [${this.desirableRooms.outwardFacingRooms.join(', ')}]`,
-      ]
-        .join('\n\n');
-    } else {
-      this.reportContent = 'Error parsing desirable rooms from unit numbers';
-    }
-  }
 }
 
 module.exports = CheckAvailabilityTableParser;
--- ReportFactory.js
+++ ReportFactory.js
@@ -1,17 +1,21 @@
 const cmd = require('node-cmd');
 
-const consoleLogReport = (content) => {
-  console.log(content);
+const consoleLogReport = (rooms) => {
+  console.log(makeFullReport(rooms));
 };
 
-const emailReport = (content) => {
+const emailReport = (rooms) => {
+  const { best } = rooms;
+  const { MAILGUN_API_KEY, MAILGUN_MAILING_LIST } = process.env;
+  if (best.length === 0) return;
+
   cmd.get(
-    `curl -s --user 'api:${process.env.MAILGUN_API_KEY}' \
+    `curl -s --user 'api:${MAILGUN_API_KEY}' \
       https://api.mailgun.net/v3/sandbox872d5518654c45bfb6c145e6dc2bc058.mailgun.org/messages \
-        -F from='Mailgun Sandbox <postmaster@sandbox872d5518654c45bfb6c145e6dc2bc058.mailgun.org>' \
-        -F to='${process.env.MAILGUN_MAILING_LIST}' \
-        -F subject='Parse Complete!' \
-        -F text="${content}"`,
+        -F from='atristryScraper@gmail.com>' \
+        -F to='${MAILGUN_MAILING_LIST}' \
+        -F subject='The Best Room Ever!' \
+        -F text="${best.join(', ')}"`,
 
     (err, data, stderr) => {
       console.log('EMAIL STATUS')
@@ -29,6 +33,21 @@ const ReporterFactory = () => {
     return emailReport;
   else
     throw 'No reporting method selected';
+}
+
+function makeFullReport(rooms) {
+  console.log(rooms)
+  if (rooms.available && rooms.best) {
+    return [
+      `Rooms: [${rooms.available.join(', ')}]`,
+      `Best rooms: [${rooms.best.join(', ')}]`,
+      `5th floor rooms: [${rooms.topFloor.join(', ')}]`,
+      `Outward facing rooms: [${rooms.outwardFacing.join(', ')}]`,
+    ]
+      .join('\n\n');
+  } else {
+    return 'Error parsing desirable rooms from unit numbers';
+  }
 }
 
 module.exports = ReporterFactory;
